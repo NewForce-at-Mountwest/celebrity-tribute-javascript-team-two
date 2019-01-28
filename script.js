46d113600a066529f5c46424460a8bf6e58c1191
Russell Miller <bet2buy@gmail.com>
2019-01-28 11:08:45 -0500

    l;ksadf

--- script.js
+++ script.js
@@ -1,66 +1,145 @@
 const linData = {
-    newsfeed: [
-      {
-        title: "Lin Manuel Miranda Working on Moana Sequel",
-        url: "https://www.cinemablend.com/news/2465663/is-lin-manuel-miranda-working-on-a-moana-sequel",
-        date: "1/23/19"
-      },
-      {
-        title: "Lin-Manuel Miranda is peeved about this year’s Oscars",
-        url: "https://pagesix.com/2019/01/25/lin-manuel-miranda-is-peeved-about-this-years-oscars/",
-        date: "1/25/19",
-      },
-      {
-        title: "Lin-Manuel Miranda in Talks to Write Disney's First Latina Disney Princess",
-        url: "https://www.thedailybeast.com/lin-manuel-miranda-in-talks-to-write-disneys-first-latina-princess",
-        date: "1/25/19",
-      },
-      {
-        title: "Puerto Rico Gives Lin-Manuel Miranda a Hero's Welcome at Hamilton Opening Weekend: 'I Felt It'",
-        url: "  https://people.com/theater/lin-manuel-miranda-hamilton-puerto-rico-standing-ovation/",
-        date: "1/25/19"
-      },
-      {
-        title: "See Lin-Manuel Miranda, ‘Hamilton’ Cast Perform ‘The Story of Tonight’ With Jimmy Fallon",
-        url: "  https://www.rollingstone.com/tv/tv-news/lin-manuel-miranda-hamilton-jimmy-fallon-779743/",
-        date: "1/16/19"
-      },
-      {
-        title: "Lin-Manuel Miranda And 'Hamilton' Partners Save A Manhattan Theater Bookstore",
-        url: "https://www.npr.org/2019/01/08/683284098/lin-manuel-miranda-and-hamilton-partners-save-a-manhattan-theater-bookstore",
-        date: "1/8/19"
-      }
-  
-    ],
-    personalLife: {
-      birthDate: "January 16, 1980",
-      birthLocation: "New York City",
-      cityOfResidence: "New York City",
-      nationality: "Puerto Rican, American",
-      family: {
-        spouse: "Vanessa Nada",
-        kids: ["Sebastian Miranda",
-          "Francisco Miranda"
-        ],
-        parents: ["Luis A. Miranda, Jr.",
-          "Luz Towns-Miranda"],
-        pets: ["Tobillo", "Toby"]
-      }
+  newsfeed: [
+    {
+      title: "Lin Manuel Miranda Working on Moana Sequel",
+      url: "https://www.cinemablend.com/news/2465663/is-lin-manuel-miranda-working-on-a-moana-sequel",
+      date: "1/23/19"
     },
-    career: {
-      shortIntro: "Lin-Manuel Miranda has written and performed in many successful musicals and movies since 2002. His most recent               musical being Hamilton: An American Musical. He recently starred in the movie, Mary Poppins Returns.",
-      MusicalsWritten: ["In The Heights", "Bring It On: The Musical", "Hamilton: An American Musical"],
-      notableRoles: ["Usnavi", "Alexander Hamilton"],
-      notableSongs: ["In The Heights", "My Shot", "Alexander Hamilton"],
-      awards: ["Multiple, Grammy Awards", "Multiple Tony Awards", "An Emmy Award"]
+    {
+      title: "Lin-Manuel Miranda is peeved about this year’s Oscars",
+      url: "https://pagesix.com/2019/01/25/lin-manuel-miranda-is-peeved-about-this-years-oscars/",
+      date: "1/25/19",
     },
-    executiveSummary: {
-      knownCollaborations: ["Dwayne The Rock Johnson", "The McElroy Brothers", "Emily Blunt", "Leslie Odom Jr.", "Daveed Diggs", "Renee         Elise Goldsberry", "Phillipa Soo"],
-      image: {
-        photURL: "https://pmcvariety.files.wordpress.com/2018/07/lin-manuel.jpg?w=1000",
-        caption: "Lin Manuel Miranda Headshot"
-      },
-      listOfAliases: ["Lin", "LMM", "Alexander Hamilton"],
-      countryOfResidence: "United States"
+    {
+      title: "Lin-Manuel Miranda in Talks to Write Disney's First Latina Disney Princess",
+      url: "https://www.thedailybeast.com/lin-manuel-miranda-in-talks-to-write-disneys-first-latina-princess",
+      date: "1/25/19",
+    },
+    {
+      title: "Puerto Rico Gives Lin-Manuel Miranda a Hero's Welcome at Hamilton Opening Weekend: 'I Felt It'",
+      url: "  https://people.com/theater/lin-manuel-miranda-hamilton-puerto-rico-standing-ovation/",
+      date: "1/25/19"
+    },
+    {
+      title: "See Lin-Manuel Miranda, ‘Hamilton’ Cast Perform ‘The Story of Tonight’ With Jimmy Fallon",
+      url: "  https://www.rollingstone.com/tv/tv-news/lin-manuel-miranda-hamilton-jimmy-fallon-779743/",
+      date: "1/16/19"
+    },
+    {
+      title: "Lin-Manuel Miranda And 'Hamilton' Partners Save A Manhattan Theater Bookstore",
+      url: "https://www.npr.org/2019/01/08/683284098/lin-manuel-miranda-and-hamilton-partners-save-a-manhattan-theater-bookstore",
+      date: "1/8/19"
     }
+
+  ],
+  personalLife: {
+    birthDate: "January 16, 1980",
+    birthLocation: "New York City",
+    cityOfResidence: "New York City",
+    nationality: "Puerto Rican, American",
+    family: {
+      spouse: "Vanessa Nada",
+      kids: ["Sebastian Miranda",
+        "Francisco Miranda"
+      ],
+      parents: ["Luis A. Miranda, Jr.",
+        "Luz Towns-Miranda"],
+      pets: ["Tobillo", "Toby"]
+    }
+  },
+
+  career: {
+    shortIntro: "Lin-Manuel Miranda has written and performed in many successful musicals and movies since 2002. His most recent               musical being Hamilton: An American Musical. He recently starred in the movie, Mary Poppins Returns.",
+    MusicalsWritten: ["In The Heights", "Bring It On: The Musical", "Hamilton: An American Musical"],
+    notableRoles: ["Usnavi", "Alexander Hamilton"],
+    notableSongs: ["In The Heights", "My Shot", "Alexander Hamilton"],
+    awards: ["Multiple, Grammy Awards", "Multiple Tony Awards", "An Emmy Award"]
+  },
+  executiveSummary: {
+    knownCollaborations: ["Dwayne The Rock Johnson", "The McElroy Brothers", "Emily Blunt", "Leslie Odom Jr.", "Daveed Diggs", "Renee         Elise Goldsberry", "Phillipa Soo"],
+    image: {
+      photURL: "https://pmcvariety.files.wordpress.com/2018/07/lin-manuel.jpg?w=1000",
+      caption: "Lin Manuel Miranda Headshot"
+    },
+    listOfAliases: ["Lin", "LMM", "Alexander Hamilton"],
+    countryOfResidence: "United States"
+  },
+  extrasReport: {
+      onlineResources: [
+          {
+              name: "Wikipedia",
+              url: "https://en.wikipedia.org/wiki/Lin-Manuel_Miranda"
+          },
+          {
+              name: "Official Website",
+              url: "http://www.linmanuel.com/"
+          },
+          {
+              name: "Fan Website",
+              url: "https://linmiranda.com/"
+          }
+      ],
+      placesToViewWork: [
+          {
+              name: "Youtube",
+              url: ""
+          },
+          {
+              name: "Broadway",
+              url: ""
+          }
+      ],
+      pastShowDates: ["January 15, 2019", "December 12, 2018", "October 7, 2018", "September 4, 2018", "August 1, 2018", "July 19, 2018", "July 2, 2018", "June 18, 2018", ]
+
   }
+}
+
+
+// personalLife: {
+//   birthDate: "January 16, 1980",
+//   birthLocation: "New York City",
+//   cityOfResidence: "New York City",
+//   nationality: "Puerto Rican, American",
+//   family: {
+//     spouse: "Vanessa Nada",
+//     kids: ["Sebastian Miranda",
+//       "Francisco Miranda"
+//     ],
+//     parents: ["Luis A. Miranda, Jr.",
+//       "Luz Towns-Miranda"],
+//     pets: ["Tobillo", "Toby"]
+//   }
+
+
+
+
+const personalHTML = `
+<h3 class="personal">Personal Life</h3>
+    <p class="personal">Lin Manuel was born on ${linData.personalLife.birthDate} in a hospital in ${linData.personalLife.birthLocation}, NY. She currently resides in ${linData.personalLife.birthLocation}, NY.  She is a ${linData.personalLife.nationality}.</p>
+   
+    <h4 class="personal">Family</h5>
+    <h4 class="personal">Spouse: ${linData.personalLife.family.spouse}</h4>
+   
+    <h5 class="personal">Kids</h5>
+    <ul class = "personal">
+        <li>${linData.personalLife.family.kids[0]}</li>
+        <li>${linData.personalLife.family.kids[1]}</li>
+    </ul>
+    
+    <h5 class="personal">Parents</h5>
+    <ul>
+        <li>${linData.personalLife.family.parents[0]}</li>
+        <li>${linData.personalLife.family.parents[1]}</li>
+    </ul>
+    
+    <h5 class="personal">Pets</h5>
+    <ul>
+        <li>${linData.personalLife.family.pets[0]}</li>
+        <li>${linData.personalLife.family.pets[1]}</li>
+    </ul>
+`
+
+
+
+document.querySelector("#personal-life").innerHTML = personalHTML;
+
