2969f67b262a0a244f898d7b43692ea6acc00023
Havey3 <45610955+Havey3@users.noreply.github.com>
2019-01-28 13:48:36 -0500

    testing pr

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
 const linData = {
-  // newsfeed: [
-  //   {
-  //     title: "Lin Manuel Miranda Working on Moana Sequel",
-  //     url: "https://www.cinemablend.com/news/2465663/is-lin-manuel-miranda-working-on-a-moana-sequel",
-  //     date: "1/23/19"
-  //   },
-  //   {
-  //     title: "Lin-Manuel Miranda is peeved about this year’s Oscars",
-  //     url: "https://pagesix.com/2019/01/25/lin-manuel-miranda-is-peeved-about-this-years-oscars/",
-  //     date: "1/25/19",
-  //   },
-  //   {
-  //     title: "Lin-Manuel Miranda in Talks to Write Disney's First Latina Disney Princess",
-  //     url: "https://www.thedailybeast.com/lin-manuel-miranda-in-talks-to-write-disneys-first-latina-princess",
-  //     date: "1/25/19",
-  //   },
-  //   {
-  //     title: "Puerto Rico Gives Lin-Manuel Miranda a Hero's Welcome at Hamilton Opening Weekend: 'I Felt It'",
-  //     url: "  https://people.com/theater/lin-manuel-miranda-hamilton-puerto-rico-standing-ovation/",
-  //     date: "1/25/19"
-  //   },
-  //   {
-  //     title: "See Lin-Manuel Miranda, ‘Hamilton’ Cast Perform ‘The Story of Tonight’ With Jimmy Fallon",
-  //     url: "  https://www.rollingstone.com/tv/tv-news/lin-manuel-miranda-hamilton-jimmy-fallon-779743/",
-  //     date: "1/16/19"
-  //   },
-  //   {
-  //     title: "Lin-Manuel Miranda And 'Hamilton' Partners Save A Manhattan Theater Bookstore",
-  //     url: "https://www.npr.org/2019/01/08/683284098/lin-manuel-miranda-and-hamilton-partners-save-a-manhattan-theater-bookstore",
-  //     date: "1/8/19"
-  //   }
+  newsfeed: [
+    {
+      title: "Lin Manuel Miranda Working on Moana Sequel",
+      url: "https://www.cinemablend.com/news/2465663/is-lin-manuel-miranda-working-on-a-moana-sequel",
+      date: "1/23/19"
+    },
+    {
+      title: "Lin-Manuel Miranda is peeved about this year’s Oscars",
+      url: "https://pagesix.com/2019/01/25/lin-manuel-miranda-is-peeved-about-this-years-oscars/",
+      date: "1/25/19",
+    },
+    {
+      title: "Lin-Manuel Miranda in Talks to Write Disney's First Latina Disney Princess",
+      url: "https://www.thedailybeast.com/lin-manuel-miranda-in-talks-to-write-disneys-first-latina-princess",
+      date: "1/25/19",
+    },
+    {
+      title: "Puerto Rico Gives Lin-Manuel Miranda a Hero's Welcome at Hamilton Opening Weekend: 'I Felt It'",
+      url: "  https://people.com/theater/lin-manuel-miranda-hamilton-puerto-rico-standing-ovation/",
+      date: "1/25/19"
+    },
+    {
+      title: "See Lin-Manuel Miranda, ‘Hamilton’ Cast Perform ‘The Story of Tonight’ With Jimmy Fallon",
+      url: "  https://www.rollingstone.com/tv/tv-news/lin-manuel-miranda-hamilton-jimmy-fallon-779743/",
+      date: "1/16/19"
+    },
+    {
+      title: "Lin-Manuel Miranda And 'Hamilton' Partners Save A Manhattan Theater Bookstore",
+      url: "https://www.npr.org/2019/01/08/683284098/lin-manuel-miranda-and-hamilton-partners-save-a-manhattan-theater-bookstore",
+      date: "1/8/19"
+    }
 
-  // ],
-  // personalLife: {
-  //   birthDate: "January 16, 1980",
-  //   birthLocation: "New York City",
-  //   cityOfResidence: "New York City",
-  //   nationality: "Puerto Rican, American",
-  //   family: {
-  //     spouse: "Vanessa Nada",
-  //     kids: ["Sebastian Miranda",
-  //       "Francisco Miranda"
-  //     ],
-  //     parents: ["Luis A. Miranda, Jr.",
-  //       "Luz Towns-Miranda"],
-  //     pets: ["Tobillo", "Toby"]
-  //   }
-  // },
-  // career: {
-  //   shortIntro: "Lin-Manuel Miranda has written and performed in many successful musicals and movies since 2002. His most recent               musical being Hamilton: An American Musical. He recently starred in the movie, Mary Poppins Returns.",
-  //   MusicalsWritten: ["In The Heights", "Bring It On: The Musical", "Hamilton: An American Musical"],
-  //   notableRoles: ["Usnavi", "Alexander Hamilton"],
-  //   notableSongs: ["In The Heights", "My Shot", "Alexander Hamilton"],
-  //   awards: ["Multiple, Grammy Awards", "Multiple Tony Awards", "An Emmy Award"]
-  // },
+  ],
+  personalLife: {
+    birthDate: "January 16, 1980",
+    birthLocation: "New York City",
+    cityOfResidence: "New York City",
+    nationality: "Puerto Rican, American",
+    family: {
+      spouse: "Vanessa Nada",
+      kids: ["Sebastian Miranda",
+        "Francisco Miranda"
+      ],
+      parents: ["Luis A. Miranda, Jr.",
+        "Luz Towns-Miranda"],
+      pets: ["Tobillo", "Toby"]
+    }
+  },
+  career: {
+    shortIntro: "Lin-Manuel Miranda has written and performed in many successful musicals and movies since 2002. His most recent               musical being Hamilton: An American Musical. He recently starred in the movie, Mary Poppins Returns.",
+    MusicalsWritten: ["In The Heights", "Bring It On: The Musical", "Hamilton: An American Musical"],
+    notableRoles: ["Usnavi", "Alexander Hamilton"],
+    notableSongs: ["In The Heights", "My Shot", "Alexander Hamilton"],
+    awards: ["Multiple, Grammy Awards", "Multiple Tony Awards", "An Emmy Award"]
+  },
   executiveSummary: {
     knownCollaborations: ["Dwayne The Rock Johnson", "The McElroy Brothers", "Emily Blunt", "Leslie Odom Jr.", "Daveed Diggs", "Renee Elise Goldsberry", "Phillipa Soo"],
     image: {
@@ -63,43 +63,67 @@ const linData = {
     listOfAliases: ["Lin", "LMM", "Alexander Hamilton"],
     countryOfResidence: "United States"
   },
-//   extrasReport: {
-//       onlineResources: [
-//           {
-//               name: "Wikipedia",
-//               url: "https://en.wikipedia.org/wiki/Lin-Manuel_Miranda"
-//           },
-//           {
-//               name: "Official Website",
-//               url: "http://www.linmanuel.com/"
-//           },
-//           {
-//               name: "Fan Website",
-//               url: "https://linmiranda.com/"
-//           }
-//       ],
-//       placesToViewWork: [
-//           {
-//               name: "Youtube",
-//               url: ""
-//           },
-//           {
-//               name: "Broadway",
-//               url: ""
-//           }
-//       ],
-//       pastShowDates: ["January 15, 2019", "December 12, 2018", "October 7, 2018", "September 4, 2018", "August 1, 2018", "July 19, 2018", "July 2, 2018", "June 18, 2018", ]
+  extrasReport: {
+      onlineResources: [
+          {
+              name: "Wikipedia",
+              url: "https://en.wikipedia.org/wiki/Lin-Manuel_Miranda"
+          },
+          {
+              name: "Official Website",
+              url: "http://www.linmanuel.com/"
+          },
+          {
+              name: "Fan Website",
+              url: "https://linmiranda.com/"
+          }
+      ],
+      placesToViewWork: [
+          {
+              name: "Youtube",
+              url: ""
+          },
+          {
+              name: "Broadway",
+              url: ""
+          }
+      ],
+      pastShowDates: ["January 15, 2019", "December 12, 2018", "October 7, 2018", "September 4, 2018", "August 1, 2018", "July 19, 2018", "July 2, 2018", "June 18, 2018", ]
 
-//   }
+  }
 }
 
 const ul_function = (title, style) => {
   return `<ul class ="${style}">${title}</ul>`
 }
 
-let htmlString = "";
+const img_function = (src, alt, style, caption) => {
+  return `<img src="${src}"class = ${style} alt="${alt}" <h1>${caption}</h1>`
+}
+
+// const img_function = (caption, alt, style, src) => {
+//   return `<h1>${caption}</h1>, <img src="${src}"class = ${style} alt="${alt}"`
+// }
+
+let execString = "";
 for (let i = 0; i < linData.executiveSummary.knownCollaborations.length; i++){
   ul_function_test = `${ul_function(linData.executiveSummary.knownCollaborations[i], "")}`
-  htmlString += ul_function_test
+  execString += ul_function_test
 }
-document.querySelector("#executive-summary").innerHTML = htmlString
+
+let imgString = "";
+img_function_test = `${img_function(linData.executiveSummary.image.photURL, "picture", "", linData.executiveSummary.image.caption)}`
+execString += img_function_test
+
+// let imgString = "";
+// img_function_test = `${img_function(linData.executiveSummary.image.caption, linData.executiveSummary.image.photURL, "picture", "")}`
+// execString += img_function_test
+
+// console.log(imgString)
+
+
+
+
+
+document.querySelector("#executive-summary").innerHTML = execString;
+
