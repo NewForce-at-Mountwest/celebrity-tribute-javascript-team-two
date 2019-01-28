889e9dc6fc7a7837d0bb9631097de7674749e7f1
Charles Belcher <belchercharles@yahoo.com>
2019-01-28 11:09:03 -0500

    working career HTML string, on to refactoring!

--- script.js
+++ script.js
@@ -47,20 +47,87 @@ const linData = {
         pets: ["Tobillo", "Toby"]
       }
     },
+
     career: {
       shortIntro: "Lin-Manuel Miranda has written and performed in many successful musicals and movies since 2002. His most recent               musical being Hamilton: An American Musical. He recently starred in the movie, Mary Poppins Returns.",
       MusicalsWritten: ["In The Heights", "Bring It On: The Musical", "Hamilton: An American Musical"],
       notableRoles: ["Usnavi", "Alexander Hamilton"],
       notableSongs: ["In The Heights", "My Shot", "Alexander Hamilton"],
-      awards: ["Multiple, Grammy Awards", "Multiple Tony Awards", "An Emmy Award"]
+      awards: ["Multiple Grammy Awards", "Multiple Tony Awards", "An Emmy Award"]
     },
-    executiveSummary: {
-      knownCollaborations: ["Dwayne The Rock Johnson", "The McElroy Brothers", "Emily Blunt", "Leslie Odom Jr.", "Daveed Diggs", "Renee         Elise Goldsberry", "Phillipa Soo"],
-      image: {
-        photURL: "https://pmcvariety.files.wordpress.com/2018/07/lin-manuel.jpg?w=1000",
-        caption: "Lin Manuel Miranda Headshot"
-      },
-      listOfAliases: ["Lin", "LMM", "Alexander Hamilton"],
-      countryOfResidence: "United States"
-    }
-  }
+}
+
+    
+//     let musicalsHTMLString = "";
+//     let rolesHTMLString = "";
+//     let songsHTMLString = "";
+//     let awardsHTMLString = "";
+
+//     for (let i = 0; i < linData.career.MusicalsWritten.length; i++) {
+//         musicalsHTMLString = musicalsHTMLString + linData.career.MusicalsWritten[i]
+//         console.log(musicalsHTMLString);
+//     }
+
+//     for (let i = 0; i < linData.career.notableRoles.length; i++) {
+//             rolesHTMLString = rolesHTMLString + linData.career.notableRoles[i]
+//             console.log(rolesHTMLString);
+//     }
+
+//     for (let i = 0; i < linData.career.notableSongs.length; i++) {
+//             songsHTMLString = songsHTMLString + linData.career.notableSongs[i]
+//             console.log(songsHTMLString);
+//     }
+
+//     for (let i = 0; i < linData.career.awards.length; i++) {
+//             awardsHTMLString = awardsHTMLString + linData.career.awards[i]
+//             console.log(awardsHTMLString);
+//     }
+    
+//     const careerHTMLString = `<h2 class="career-intro">${linData.career.shortIntro}</h2> <ul class="career-musicals"${musicalsHTMLString}</ul> <ul class="career-roles">${rolesHTMLString}</ul> <ul class="career-songs">${songsHTMLString}</ul> <ul class="career-awards">${awardsHTMLString}</ul>`
+//     console.log (careerHTMLString) 
+
+// document.querySelector("#career").innerHTML = careerHTMLString;
+
+
+
+
+
+//     executiveSummary: {
+//       knownCollaborations: ["Dwayne The Rock Johnson", "The McElroy Brothers", "Emily Blunt", "Leslie Odom Jr.", "Daveed Diggs", "Renee         Elise Goldsberry", "Phillipa Soo"],
+//       image: {
+//         photURL: "https://pmcvariety.files.wordpress.com/2018/07/lin-manuel.jpg?w=1000",
+//         caption: "Lin Manuel Miranda Headshot"
+//       },
+//       listOfAliases: ["Lin", "LMM", "Alexander Hamilton"],
+//       countryOfResidence: "United States"
+//     }
+//   }
+//   extrasReport: {
+//     onlineResources: [
+//         {
+//             name: "Wikipedia",
+//             url: "https://en.wikipedia.org/wiki/Lin-Manuel_Miranda"
+//         },
+//         {
+//             name: "Official Website",
+//             url: "http://www.linmanuel.com/"
+//         },
+//         {
+//             name: "Fan Website",
+//             url: "https://linmiranda.com/"
+//         }
+//     ],
+//     placesToViewWork: [
+//         {
+//             name: "Youtube",
+//             url: ""
+//         },
+//         {
+//             name: "Broadway",
+//             url: ""
+//         }
+//     ],
+//     pastShowDates: ["January 15, 2019", "December 12, 2018", "October 7, 2018", "September 4, 2018", "August 1, 2018", "July 19, 2018", "July 2, 2018", "June 18, 2018", ]
+
+// }
+// }
